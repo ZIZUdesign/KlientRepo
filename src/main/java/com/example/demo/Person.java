abf27e951e6932afd360330fdbe0a588f43792d8
ZIZUdesign <55802666+ZIZUdesign@users.noreply.github.com>
2021-01-08 04:04:19 +0100

    WebProg

--- src/main/java/com/example/demo/Person.java
+++ src/main/java/com/example/demo/Person.java
@@ -3,10 +3,31 @@ package com.example.demo;
 public class Person {
 
     private String navn;
+    private int Id;
 
-    public Person (String navn){
-        this.navn=navn;
+    public String getNavn() {
+        return navn;
     }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public Person (String navn, int id ){
+
+        this.navn=navn;
+        this.Id= id;
+    }
+
+
 
 
 }
